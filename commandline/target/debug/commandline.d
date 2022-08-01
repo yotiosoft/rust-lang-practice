dc460f9c1c6d3677adebcb31aa4cc279f54813fb
YotioSoft <ytani0323@gmail.com>
2022-08-01 22:21:19 +0900

    add commandline

---
+++ commandline/target/debug/commandline.d
@@ -0,0 +1,1 @@
+/Users/ytani/git/rust-lang/commandline/target/debug/commandline: /Users/ytani/git/rust-lang/commandline/src/main.rs
---
+++ commandline/target/debug/deps/commandline-a756809053433446.d
@@ -0,0 +1,5 @@
+/Users/ytani/git/rust-lang/commandline/target/debug/deps/commandline-a756809053433446: src/main.rs
+
+/Users/ytani/git/rust-lang/commandline/target/debug/deps/commandline-a756809053433446.d: src/main.rs
+
+src/main.rs:
