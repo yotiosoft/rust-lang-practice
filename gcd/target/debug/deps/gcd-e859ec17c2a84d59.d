074f62dbb49a58d8691d8e4acef420d09c5c4928
YotioSoft <ytani0323@gmail.com>
2022-08-01 21:53:28 +0900

    add gcd

---
+++ gcd/target/debug/deps/gcd-e859ec17c2a84d59.d
@@ -0,0 +1,5 @@
+/Users/ytani/git/rust-lang/gcd/target/debug/deps/gcd-e859ec17c2a84d59: src/main.rs
+
+/Users/ytani/git/rust-lang/gcd/target/debug/deps/gcd-e859ec17c2a84d59.d: src/main.rs
+
+src/main.rs:
---
+++ gcd/target/debug/gcd.d
@@ -0,0 +1,1 @@
+/Users/ytani/git/rust-lang/gcd/target/debug/gcd: /Users/ytani/git/rust-lang/gcd/src/main.rs
