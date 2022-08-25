fff03a478405edbd99c469ab0ab2716efcb8ef31
YotioSoft <ytani0323@gmail.com>
2022-08-26 01:42:30 +0900

    mut ref : add sort_works()

--- fragments/src/main.rs
+++ fragments/src/main.rs
@@ -2,10 +2,16 @@ use std::collections::HashMap;
 
 type Table = HashMap<String, Vec<String>>;
 
+fn sort_works(table: &mut Table) {
+    for (_artist, works) in table {
+        works.sort();
+    }
+}
+
 fn show(table: &Table) {
-    for (artist, works) in table {
+    for (artist, works) in table {          // artist: &String, works: &Vec<String>
         println!("works by {}:", artist);
-        for work in works {
+        for work in works {                 // work: &String
             println!(" {}", work);
         }
     }
@@ -17,5 +23,7 @@ fn main() {
     table.insert("Caravaggio".to_string(), vec!["The Musicians".to_string(), "The Calling of St. Matthew".to_string()]);
     table.insert("Cellini".to_string(), vec!["Perseus with the head of Medusa".to_string(), "a salt celler".to_string()]);
 
-    show(&table);        // 参照
+    sort_works(&mut table);     // 可変参照（sort_worksはtableを変更できる）
+
+    show(&table);               // 共有参照（showはtableを変更できない）
 }
