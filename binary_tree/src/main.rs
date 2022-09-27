2a008eadfa7fb3eecb53895470b4fe0197ea6844
YotioSoft <ytani0323@gmail.com>
2022-09-28 03:02:21 +0900

    complete binary_tree

--- binary_tree/src/main.rs
+++ binary_tree/src/main.rs
@@ -7,6 +7,27 @@ enum BinaryTree<T> {
     NonEmpty(Box<TreeNode<T>>),
 }
 
+impl<T: Ord> BinaryTree<T> {
+    fn add(&mut self, value: T) {
+        match *self {
+            BinaryTree::Empty => {
+                *self = BinaryTree::NonEmpty(Box::new(TreeNode {
+                    element: value,
+                    left: BinaryTree::Empty,
+                    right: BinaryTree::Empty,
+                }))
+            }
+            BinaryTree::NonEmpty(ref mut node) => {
+                if value <= node.element {
+                    node.left.add(value);
+                } else {
+                    node.right.add(value);
+                }
+            }
+        }
+    }
+}
+
 // BinaryTreeの一部
 struct TreeNode<T> {
     element: T,
