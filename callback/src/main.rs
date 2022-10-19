12d27b1dde84c9eb071e15014e2689951e3e7782
YotioSoft <ytani0323@gmail.com>
2022-10-19 14:59:06 +0900

    temp

--- callback/src/main.rs
+++ callback/src/main.rs
@@ -1,18 +1,15 @@
-extern crate iron;
-extern crate router;
-extern crate urlencoded;
-#[macro_use] extern crate mime; // このクレートでエクスポートされているマクロを使用
+// reference from Programming Rust ver.2
 
-use iron::prelude::*;           // iron::preludeで公開されているすべての名前を利用
-use iron::status;
-use router::Router;
-use urlencoded::UrlEncodedBody;
 use std::str::FromStr;
+use std::env;
+use actix_web::{web, App, HttpResponse, HttpServer};
+use serde::Deserialize;
 
+// actix-gcdのルータを自前で作成
 struct Request {
     method: String,
     url: String,
-    header: HashMap<String, String>,
+    headers: HashMap<String, String>,
     body: Vec<u8>
 }
 
@@ -27,12 +24,12 @@ struct BasicRouter<C> where C: Fn(&Request) -> Response {
 }
 
 impl<C> BasicRouter<C> where C: Fn(&Request) -> Response {
-    // 空のルータを作成
+    /// 空のルータを作成
     fn new() -> BasicRouter<C> {
         BasicRouter { routes: HashMap::new() }
     }
 
-    // ルータにルートを追加
+    /// ルータにルートを追加
     fn add_route(&mut self, url: &str, callback: C) {
         self.routes.insert(url.to_string(), callback);
     }
@@ -40,24 +37,40 @@ impl<C> BasicRouter<C> where C: Fn(&Request) -> Response {
 
 fn main() {
     let mut router = BasicRouter::new();
-    router.add_route("/", |_| get_from_response());
+    router.add_route("/", |_| web::get().to(get_index));
 }
 
-fn get_form(_request: &mut Request) -> IronResult<Response> {   //_requestは後で使う(_で使わないことをコンパイラに示す)
-    let mut response = Response::new();
+fn get_index() -> HttpResponse {
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(
+            r#"
+                <title>GCD Calculator</title>
+                <form action="/gcd" method="post">
+                <input type="text" name="n"/>
+                <input type="text" name="m"/>
+                <button type="submit">Compute GCD</button>
+                </form>
+            "#,
+        )
+}
 
-    response.set_mut(status::Ok);                           // srarus::Ok -> HTTPステータス
-    response.set_mut(mime!(Text/Html; Charset=Utf8));       // メディアタイプ -> Content-Typeヘッダ
-    response.set_mut(r#"
-        <title>GCD Calculator</title>
-        <form action="/gcd" method="post">
-            <input type="text" name="n"/>
-            <input type="text" name="n"/>
-            <button type="submit">Compute GCD</button>
-        </form>
-    "#);                                                    // 文字列 -> レスポンスのボディ部分 (r#" .. #"で複数行に渡って記述(JavaScriptの`のようなもの))
+#[derive(Deserialize)]
+struct GcdParameters {
+    n: u64,
+    m: u64,
+}
 
-    Ok(response)
+fn post_gcd(form: web::Form<GcdParameters>) -> HttpResponse {
+    if form.n == 0 || form.m == 0 {
+        return HttpResponse::BadRequest()
+            .content_type("text/html")
+            .body("Computing the GCD with zero is boring.");
+    }
+
+    let response = format!("The gratest common divisor og the numbers {} and {} is <b>{}</b>\n", form.n, form.m, gcd(form.n, form.m));
+
+    HttpResponse::Ok().content_type("text/html").body(response)
 }
 
 fn gcd(mut n: u64, mut m: u64) -> u64 {
@@ -73,55 +86,9 @@ fn gcd(mut n: u64, mut m: u64) -> u64 {
     n
 }
 
-fn post_gcd(request: &mut Request) -> IronResult<Response> {
-    let mut response = Response::new();
-
-    // パターンマッチング
-    let form_data = match request.get_ref::<UrlEncodedBody>() {      // リクエストのボディをパース
-        Err(e) => {                                                               // エラー処理（エラーメッセージを返す）
-            response.set_mut(status::BadRequest);
-            response.set_mut(format!("Error parsing from data: {:?}\n", e));
-            return Ok(response);
-        }
-        Ok(map) => map                                               // 成功時、クエリのパラメータ名と値の配列をテーブルに変換
-    };
-
-    let unparsed_numbers = match form_data.get("n") {                                 // クエリパラメータ'n'があるか？
-        None => {                                                                                   // なければエラーメッセージを返す
-            response.set_mut(status::BadRequest);
-            response.set_mut(format!("form data has no 'n' parameter\n"));
-            return Ok(response);
-        }
-        Some(nums) => nums                                                            // あればその文字列ベクタを返す
-    };
-
-    let mut numbers = Vec::new();                                                         // 文字列ベクタを1つずつパース -> 符号なし64ビット変数に
-    for unparsed in unparsed_numbers {
-        match u64::from_str(&unparsed) {
-            Err(_) => {                                                                             // 1つでも失敗したらエラー
-                response.set_mut(status::BadRequest);
-                response.set_mut(
-                    format!("Value for 'n' parameter not a number: {:?}\n", 
-                    unparsed));
-                return Ok(response);
-            }
-            Ok(n) => { numbers.push(n); }                                                      // 成功したらu64ベクタへ
-        }
-    }
-
-    // gcdを求める
-    let mut d = numbers[0];
-    for m in &numbers[1..] {
-        d = gcd(d, *m);
-    }
-
-    // レスポンスを生成
-    response.set_mut(status::Ok);
-    response.set_mut(mime!(Text/Html; Charset=Utf8));
-    response.set_mut(
-        format!("The gratest common divisor of the numbers {:?} is <b>{}</b>\n",
-                            numbers, d)         // format!: ストリームにテキストを書き出さずに文字列自体を返すマクロ
-    );
-    Ok(response)
+#[test] // cargo test
+fn test_gcd() {
+    assert_eq!(gcd(14, 15), 1);
+    
+    assert_eq!(gcd(2 * 3 * 5 * 11 * 17, 3 * 7 * 11 * 13 * 19), 3 * 11);
 }
-
