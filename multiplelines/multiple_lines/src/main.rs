509c1c90659d8a5cff835bcdc65be0374d4a56a9
YotioSoft <ytani0323@gmail.com>
2023-02-10 01:43:16 +0900

    add other way

--- multiplelines/multiple_lines/src/main.rs
+++ multiplelines/multiple_lines/src/main.rs
@@ -46,3 +46,35 @@ fn sleep(millis: u64) {
     let duration = time::Duration::from_millis(millis);
     thread::sleep(duration);
 }
+
+///other way
+/*use std::io::{self, BufRead};
+
+fn main() -> io::Result<()> {
+    let mut lines = io::stdin().lock().lines();
+    let mut user_input = String::new();
+
+    while let Some(line) = lines.next() {
+        let last_input = line.unwrap();
+
+        // stop reading
+        if last_input.len() == 0 {
+            break;
+        }
+
+        // add a new line once user_input starts storing user input
+        if user_input.len() > 0 {
+            user_input.push_str("\n");
+        }
+
+        // store user input
+        user_input.push_str(&last_input);
+    }
+
+    println!("\nMulti-line user input \n{}", user_input);
+
+    // the lock is released after it goes out of scope
+    Ok(())
+}
+*/
+
